246b8bfe400075cb1a022f59fc408b47d7432ef8
magurouhiru <asdflkj169@gmail.com>
2026-03-21 08:46:13 +0000

    fix

--- packages/asb-web/src/routes/calc_level.tsx
+++ packages/asb-web/src/routes/calc_level.tsx
@@ -38,7 +38,7 @@ const items = SAFE_DICT.map((n) => ({ id: n.en as Key, name: n.ja })).sort(
 const fuzzySearch = (searchName: string) => {
   const fuse = new Fuse(items, {
     keys: ["name"],
-    threshold: ,
+    threshold: 1,
   });
   const result = fuse.search(searchName);
   return result[0]?.item.name ?? "";
